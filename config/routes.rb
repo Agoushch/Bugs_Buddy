9bad07401841f261f011b28f6b2c22f82aa2f802
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-24 13:02:45 +0100

    dashbo v3 pas fini

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :activities, only: %i[show new create destroy] do
+    resources :sports
     resources :user_activities do
       resources :comments
     end
-    end
   end
-  resources :sports
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
