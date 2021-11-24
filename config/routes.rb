dc6a03c89f5e12b43b7acd4fde092a5a88de7861
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-24 12:46:50 +0100

    dashboard no fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :activities, only: %i[show new create destroy]
+  resources :activities, only: %i[show new create destroy] do
+    resources :user_activities do
+      resources :comments
+    end
+    end
+  end
   resources :sports
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
