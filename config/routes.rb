165dfa6f9af1fcd2c3f3491052dcb04f3d062b79
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-24 15:36:35 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :activities, only: %i[show new create destroy]
-  resources :sports
+  resources :activities, only: %i[show new create destroy] do
+    resources :sports
+    resources :user_activities do
+      resources :comments
+    end
+  end
   get "dashboard", to: "pages#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
