d384e5be4ae2305678adb827a3244da56cdfc9a0
Thibaut Butaye <thibautbutaye@gmail.com>
2021-11-24 12:11:31 +0100

    footer revu

--- app/controllers/user_activities_controller.rb
+++ app/controllers/user_activities_controller.rb
@@ -1,2 +1,10 @@
 class UserActivitiesController < ApplicationController
+  def create
+    @user_activity = UserActivity.new
+  end
+
+  def destroy
+    @user_activity.delete
+    redirect_to user_path
+  end
 end
