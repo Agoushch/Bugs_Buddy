3af7e2ba186db59b7ccac720dabc91c7cab2b51f
adonis goushchi <amidonis@hotmail.com>
2021-11-26 16:28:27 +0100

    category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_105253) do
+ActiveRecord::Schema.define(version: 2021_11_26_152428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2021_11_26_105253) do
 
   create_table "sports", force: :cascade do |t|
     t.string "kind"
-    t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id", null: false
