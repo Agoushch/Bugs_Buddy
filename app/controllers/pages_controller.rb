a50d3f616f656f5b6aa89d40ad786e80dc1d2947
Filip Brouwers <filip.brouwers@gmail.com>
2021-11-29 17:41:25 +0100

    dashboard trié, css to do

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,6 @@ class PagesController < ApplicationController
 
   def dashboard
     @activities_mine = current_user.activities
-
-    raise
+    @activities = current_user.user_activities.sort_by { |act| Activity.find_by(id: act.id).date}
   end
 end
