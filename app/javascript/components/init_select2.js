10800e1f41653463d42baee2d62c3937d0d25b92
Filip Brouwers <filip.brouwers@gmail.com>
2021-12-03 15:36:00 +0100

    button fix

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -6,7 +6,7 @@ import 'select2';
 
   const initSelect2 = () => {
   $('.select2').select2({
-    placeholder: "-- Select the sport for which you would like to add an activity --",
+    placeholder: "Select the sport for which you would like to add an activity",
     // width: 100%
   });
   };
